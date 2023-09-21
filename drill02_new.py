987662542624ba473a6c80afa335452dca47ce92
Jung-Uiseok <tnekwjd3359@naver.com>
2023-09-21 09:54:48 +0900

    def func run_circle and run_rectangle

--- drill02_new.py
+++ drill02_new.py
@@ -5,6 +5,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():
+    pass
+
+def run_rectangle():
+    pass
+
 while True:
     run_circle()
     run_rectangle()
